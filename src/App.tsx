7960b949add1cadfc811964aa190156ec1663ddc
Henrique <henrique.scastro12@gmail.com>
2025-03-20 01:39:11 -0300

    Projeto base inicial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "src/components/ui/button";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-red-500 text-4xl font-bold">Projeto Base</h1>
+      <Button variant="outline">Button</Button>
+    </div>
+  );
+}
+
+export default App;
